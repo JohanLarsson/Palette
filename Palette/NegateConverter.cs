a24ab7c1eac45f6f78989fae3c0b5c0fb60aefe0
Johan Larsson <johan_gavle1@hotmail.com>
2017-04-22 23:17:55 +0200

    Dots.

---
+++ Palette/NegateConverter.cs
@@ -0,0 +1,21 @@
+﻿namespace Palette
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class NegateConverter : IValueConverter
+    {
+        public static readonly NegateConverter Default = new NegateConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return -1 * (double) value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return -1 * (double)value;
+        }
+    }
+}
---
+++ Palette/SaturationToRadiusConverter.cs
@@ -0,0 +1,27 @@
+﻿namespace Palette
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+    using System.Windows.Markup;
+
+    public class SaturationToRadiusConverter : MarkupExtension , IValueConverter
+    {
+        public double Radius { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double) value * Radius;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)value / Radius;
+        }
+    }
+}
